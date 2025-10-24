38655b28262704db01bc3115bb4ad1cd8f9a8829
Vignesh <vigneshantony5@gmail.com>
2025-10-25 01:01:06 +0530

    fix: initialize sentry before app loads

--- src/instrument.ts
+++ src/instrument.ts
@@ -5,10 +5,11 @@ import {
   createRoutesFromChildren,
   useNavigationType,
   matchRoutes,
-} from "react-router-dom";
+} from "react-router";
 
 Sentry.init({
   dsn: "https://9dd6874578e0d756aed653a36b016a7f@o4510244748722176.ingest.us.sentry.io/4510244749901824",
+  environment: "development",
   sendDefaultPii: true,
   integrations: [
     Sentry.reactRouterV7BrowserTracingIntegration({
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./instrument.ts";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./instrument.ts";
 
 const queryClient = new QueryClient();
 
